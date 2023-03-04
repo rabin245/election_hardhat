31ab24e66306b7d2f872c3d618191b620038883c
rabin245 <drarabin@gmail.com>
2023-03-04 21:34:25 +0545

    added more candidate related functions

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,20 +2,18 @@
 pragma solidity 0.8.17;
 
 // Errors
-error Election__NotOwner(address sender, string error);
+// error Election__NotOwner(address sender, string error);
+error Election__NoCandidateWithGivenId(uint256 id);
 
 contract Election {
     // type declarations
     struct Candidate {
         uint256 id;
         string name;
+        string partyName;
+        string imageUrl;
         uint256 votes;
     }
-
-    // struct Voter{
-    //     address voterAddress;
-    //     bool hasVoted;
-    // }
 
     // state variables
     Candidate[] private s_candidates;
@@ -33,12 +31,7 @@ contract Election {
 
     // Modifiers
     modifier onlyOwner() {
-        if (msg.sender != i_owner) {
-            revert Election__NotOwner({
-                sender: msg.sender,
-                error: "You are not the owner!"
-            });
-        }
+        require(msg.sender == i_owner, "You are not the owner!");
         _; // run rest of the lines
     }
 
@@ -55,6 +48,11 @@ contract Election {
         _;
     }
 
+    modifier electionHasNotStarted() {
+        require(!s_hasStarted, "Election has already started!");
+        _;
+    }
+
     // constructor
     constructor() {
         i_owner = msg.sender;
@@ -65,15 +63,15 @@ contract Election {
     function startElection() public onlyOwner {
         require(s_hasStarted == false, "Already started!");
         s_hasStarted = true;
+        emit electionStarted();
         emptyCandidates();
         setCandidates();
-        emit electionStarted();
     }
 
     function endElection() public onlyOwner {
         require(s_hasStarted == true, "Already ended!");
-        s_hasStarted = false;
         emit electionEnded();
+        s_hasStarted = false;
     }
 
     // not really needed right now
@@ -81,21 +79,77 @@ contract Election {
         s_voters.push(msg.sender);
     }
 
-    function voteToCandidate(uint256 id) public onlyOnce electionHasStarted {
+    function voteToCandidate(uint256 id) public electionHasStarted onlyOnce {
         s_voterToCandidateId[msg.sender] = id;
-        s_candidates[id - 1].votes++;
-        emit voteCasted(s_candidates[id - 1]);
+        uint256 candidateIndex = getCandidateIndex(id);
+        s_candidates[candidateIndex].votes++;
+        emit voteCasted(s_candidates[candidateIndex]);
+    }
+
+    function addCandidates(
+        uint256 id,
+        string calldata name,
+        string calldata partyName
+    )
+        public
+        // string calldata imageUrl
+        onlyOwner
+        electionHasNotStarted
+    {
+        s_candidates.push(
+            Candidate({
+                id: id,
+                name: name,
+                partyName: partyName,
+                imageUrl: "https://picsum.photos/400",
+                votes: 0
+            })
+        );
+    }
+
+    function removeCandidate(
+        uint256 id
+    ) public onlyOwner electionHasNotStarted {
+        uint256 delIndex = getCandidateIndex(id);
+
+        if (delIndex >= s_candidates.length) {
+            revert Election__NoCandidateWithGivenId(id);
+        }
+
+        for (uint256 i = delIndex; i < s_candidates.length - 1; i++) {
+            s_candidates[i] = s_candidates[i + 1];
+        }
+        s_candidates.pop();
     }
 
     // private
     function setCandidates() private {
         for (uint256 i = 0; i < 5; i++) {
-            s_candidates.push(Candidate({id: i + 1, name: "Name", votes: 0}));
+            s_candidates.push(
+                Candidate({
+                    id: i + 1,
+                    name: "Name",
+                    partyName: "party",
+                    imageUrl: "https://picsum.photos/400",
+                    votes: 0
+                })
+            );
         }
     }
 
     function emptyCandidates() private {
         delete s_candidates;
+    }
+
+    function getCandidateIndex(uint256 id) private view returns (uint256) {
+        Candidate[] memory candidatesList = s_candidates;
+        for (uint256 i = 0; i < candidatesList.length; i++) {
+            Candidate memory cand = candidatesList[i];
+            if (cand.id == id) {
+                return i;
+            }
+        }
+        revert Election__NoCandidateWithGivenId(id);
     }
 
     // view / pure
@@ -125,7 +179,14 @@ contract Election {
 
     function getCandidate(
         uint256 id
-    ) public view electionHasStarted returns (Candidate memory) {
-        return s_candidates[id - 1];
+    ) public view electionHasStarted returns (Candidate memory candidate) {
+        Candidate[] memory candidatesList = s_candidates;
+        for (uint256 i = 0; i < candidatesList.length; i++) {
+            Candidate memory cand = candidatesList[i];
+            if (cand.id == id) {
+                return cand;
+            }
+        }
+        revert Election__NoCandidateWithGivenId(id);
     }
 }
